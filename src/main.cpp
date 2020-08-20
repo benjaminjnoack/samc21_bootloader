6e98ead21409b8e4e4645f06e2bcdc1c36d71bbe
ben <bnoack@techshot.com>
2020-08-20 14:38:53 -0400

    trying something

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
-#include <iostream>
+#include "sam.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#define LED_PIN PORT_PA15
+
+int main(void)
+{
+    SystemInit();
+
+    REG_PORT_DIR0 |= LED_PIN;//set the direction to output
+    REG_PORT_OUT0 |= LED_PIN;//set the pin high (LED off)
+
+    while (1) 
+    {
+	    for (uint32_t i = 0; i < 100000; i++)
+		    ;
+
+    	REG_PORT_OUTTGL0 |= LED_PIN;
+    }
 }
+
