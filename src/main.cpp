1a494feae36e9965bf9870fe60f7d87e91125749
ben <bnoack@techshot.com>
2020-08-21 11:14:34 -0400

    memory_map

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include "sam.h"
+#include "memory_map.h"
 
 #define LED_PIN PORT_PA15
 
 int main(void)
 {
     SystemInit();
+    uint32_t *app_code = (uint32_t *) __app_rom_start__;
+    uint32_t app_sp = app_code[0];
 
     REG_PORT_DIR0 |= LED_PIN;//set the direction to output
     REG_PORT_OUT0 |= LED_PIN;//set the pin high (LED off)
---
+++ src/memory_map.h
@@ -0,0 +1,9 @@
+#ifndef SAMC21_BOOTLOADER_MEMORY_MAP_H
+#define SAMC21_BOOTLOADER_MEMORY_MAP_H
+
+extern int __boot_rom_start__;
+extern int __boot_rom_length__;
+extern int __app_rom_start__;
+extern int __app_rom_length__;
+
+#endif //SAMC21_BOOTLOADER_MEMORY_MAP_H
