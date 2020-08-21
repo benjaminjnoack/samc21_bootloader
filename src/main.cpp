c1de153605802c2a9533a50e556e0500ad201156
ben <bnoack@techshot.com>
2020-08-21 16:34:08 -0400

    extern uint32_t

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,27 @@
+#include <cstdint>
 #include "sam.h"
 #include "memory_map.h"
 
 #define LED_PIN PORT_PA15
 
+
 int main(void)
 {
     SystemInit();
-    int *app_code = &__app_rom_start__;
-    int *app_len = &__app_rom_length__;
+
+    uint32_t *app_code = (uint32_t *) __app_rom_start__;
+    uint32_t app_sp = app_code[0];
+    uint32_t app_start = app_code[1];
 
     REG_PORT_DIR0 |= LED_PIN;//set the direction to output
     REG_PORT_OUT0 |= LED_PIN;//set the pin high (LED off)
 
-    while (1) 
+    while (1)
     {
-	    for (uint32_t i = 0; i < 100000; i++)
-		    ;
+        for (uint32_t i = 0; i < 100000; i++)
+            ;
 
-    	REG_PORT_OUTTGL0 |= LED_PIN;
+        REG_PORT_OUTTGL0 |= LED_PIN;
     }
 }
 
--- src/memory_map.h
+++ src/memory_map.h
@@ -1,9 +1,9 @@
 #ifndef SAMC21_BOOTLOADER_MEMORY_MAP_H
 #define SAMC21_BOOTLOADER_MEMORY_MAP_H
 
-extern int __boot_rom_start__;
-extern int __boot_rom_length__;
-extern int __app_rom_start__;
-extern int __app_rom_length__;
+extern uint32_t __boot_rom_start__;
+extern uint32_t __boot_rom_length__;
+extern uint32_t __app_rom_start__;
+extern uint32_t __app_rom_length__;
 
 #endif //SAMC21_BOOTLOADER_MEMORY_MAP_H
