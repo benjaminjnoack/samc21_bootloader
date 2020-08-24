26d3dbb9fb54f21598cee7b77c141a0ad4c9817a
ben <bnoack@techshot.com>
2020-08-24 16:59:18 -0400

    asm

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,13 @@
 
 #define LED_PIN PORT_PA14
 
-void boot_app(uint32_t pc, uint32_t sp);
+/**
+ * https://static.docs.arm.com/ihi0042/g/aapcs32.pdf
+ * section 6.1.1 Core registers
+ * "The first four registers r0-r3 (a1-a4) are used to pass argument values into a subroutine..."
+ * TODO __attribute__((noinline,noreturn))
+ */
+void boot_app(uint32_t sp, uint32_t pc);
 
 int main(void)
 {
@@ -15,9 +21,9 @@ int main(void)
 
     uint32_t *app_code = (uint32_t *) __app_rom_start__;
     uint32_t app_sp = app_code[0];
-    uint32_t app_start = app_code[1];
+    uint32_t app_pc = app_code[1];
 
-    boot_app(app_sp, app_start);
+    boot_app(app_pc, app_sp);
 
     while (1)
     {
@@ -28,14 +34,13 @@ int main(void)
     }
 }
 
-void boot_app(uint32_t pc, uint32_t sp)
+/*
+ * TODO would be nice to "use" the variables in C as to avoid the unused-variable warning
+ */
+void boot_app(uint32_t sp, uint32_t pc)
 {
-    /**
-     * https://static.docs.arm.com/ihi0042/g/aapcs32.pdf
-     * section 6.1.1 Core registers
-     * "The first four registers r0-r3 (a1-a4) are used to pass argument values into a subroutine..."
-     * TODO would be nice to "use" the variables in C as to avoid the unused-variable warning
-     */
-    __asm("msr msp, r1\n"
-          "bx r0");
+    __asm("mov r0, #0x20002478\n"
+          "mov r1, #0x0000415d\n"
+          "msr msp, r0\n"
+          "bx r1");
 }
