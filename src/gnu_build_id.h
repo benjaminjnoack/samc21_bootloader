0a9e066f7ef3063255a74518ff57f05db23c5c03
ben <bnoack@techshot.com>
2020-08-25 16:13:24 -0400

    read build id

---
+++ src/gnu_build_id.h
@@ -0,0 +1,21 @@
+/**
+ * https://interrupt.memfault.com/blog/gnu-build-id-for-firmware
+ */
+#ifndef SAMC21_BOOTLOADER_GNU_BUILD_ID_H
+#define SAMC21_BOOTLOADER_GNU_BUILD_ID_H
+
+#include "stdint.h"
+
+struct ElfNoteSection {
+    uint32_t namesz;
+    uint32_t descsz;
+    uint32_t type;
+    uint8_t data[];
+};
+
+/**
+ * provided by bootloader.ld
+ */
+extern const struct ElfNoteSection __gnu_build_id__;
+
+#endif //SAMC21_BOOTLOADER_GNU_BUILD_ID_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <cstdint>
 #include "sam.h"
 #include "memory_map.h"
+#include "gnu_build_id.h"
 
 #define LED_PIN PORT_PA14
 
@@ -18,22 +19,44 @@ int main(void)
     REG_PORT_DIR0 |= LED_PIN;//set the direction to output
     REG_PORT_OUT0 |= LED_PIN;//set the pin high (LED off)
 
+    uint32_t i;
+    uint32_t sz = __gnu_build_id__.namesz;
+    uint8_t data = 0;
+
+    /**
+     * There are FAR easier ways to iterate through this data.
+     * However, we aren't actually using the data,
+     * and this prevents the compiler from optimizing it away
+     */
+    for (i = 0; i < sz; i++) {
+        data = __gnu_build_id__.data[i];
+        if (data == '\0') {
+            break;
+        }
+    }
+
+    sz = __gnu_build_id__.namesz + __gnu_build_id__.descsz;
+    for (i++; i < sz; i++) {
+        data = __gnu_build_id__.data[i];
+        if (data == '\0') {
+            break;
+        }
+    }
+
+
     /**
      * __app_rom_start__ is declared in memory_map.h
      * which, in turn, is defined in memory_map.ld
-     */
     uint32_t *app_start = &__app_rom_start__;
     uint32_t sp = app_start[0];
     uint32_t pc = app_start[1];
-
-    /**
-     * TODO inline extended asm
-     */
     boot_app(sp, pc);
+     */
+
 
     while (1)
     {
-        for (uint32_t i = 0; i < 100000; i++)
+        for (i = 0; i < 100000; i++)
             ;
 
         REG_PORT_OUTTGL0 |= LED_PIN;
