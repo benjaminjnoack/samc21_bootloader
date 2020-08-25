511c2afaf806f4e923811c252e2eeebef0f3f0e4
ben <bnoack@techshot.com>
2020-08-25 10:54:30 -0400

    comments

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,10 @@
 #define LED_PIN PORT_PA14
 
 /**
- * https://static.docs.arm.com/ihi0042/g/aapcs32.pdf
- * section 6.1.1 Core registers
- * "The first four registers r0-r3 (a1-a4) are used to pass argument values into a subroutine..."
- */
+* https://static.docs.arm.com/ihi0042/g/aapcs32.pdf
+* section 6.1.1 Core registers
+* "The first four registers r0-r3 (a1-a4) are used to pass argument values into a subroutine..."
+*/
 __attribute__((naked)) void boot_app(uint32_t sp, uint32_t pc);
 
 int main(void)
@@ -21,6 +21,7 @@ int main(void)
     uint32_t *app_start = &__app_rom_start__;
     uint32_t sp = app_start[0];
     uint32_t pc = app_start[1];
+
     boot_app(sp, pc);
 
     while (1)
@@ -32,9 +33,6 @@ int main(void)
     }
 }
 
-/*
- * TODO would be nice to "use" the variables in C as to avoid the unused-variable warning
- */
 void boot_app(uint32_t sp, uint32_t pc)
 {
     __asm("msr msp, r0\n"
