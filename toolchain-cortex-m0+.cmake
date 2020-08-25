e191c6ba071860d959db6bb32568e2e718bd2a30
ben <bnoack@techshot.com>
2020-08-25 12:29:23 -0400

    Squashed 'samc21_cmsis/' changes from b28d5a3..80c63df
    80c63df warn
    
    git-subtree-dir: samc21_cmsis
    git-subtree-split: 80c63df8ea842cbf48814e24f1d0686d0b0a2a09

--- toolchain-cortex-m0+.cmake
+++ toolchain-cortex-m0+.cmake
@@ -5,7 +5,8 @@ set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
 set(OBJCOPY arm-none-eabi-objcopy)
 set(NM arm-none-eabi-nm)
 
-set(COMMON_FLAGS "-Wall -mcpu=cortex-m0plus -mthumb -mthumb-interwork -ffunction-sections -fdata-sections -fno-common -specs=\"nosys.specs\"")
+set(COMPILER_WARNINGS "-Wall -Wextra -Werror"
+set(COMMON_FLAGS "${COMPILER_WARNINGS} -mcpu=cortex-m0plus -mthumb -mthumb-interwork -ffunction-sections -fdata-sections -fno-common -specs=\"nosys.specs\"")
 
 set(CMAKE_C_FLAGS_INIT ${COMMON_FLAGS})
 set(CMAKE_C_FLAGS_RELEASE "-Os")
