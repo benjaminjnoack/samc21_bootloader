39c4547eeb08a416d0364a305458dda9178760ff
Benjamin Noack <benjamin.noack@icloud.com>
2020-08-30 00:05:27 -0400

    Core in samc21

--- samc21/samc21.cmake
+++ samc21/samc21.cmake
@@ -1,15 +1,15 @@
 cmake_minimum_required(VERSION 3.17)
 
-include(toolchain-cortex-m0+.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/toolchain-cortex-m0+.cmake)
 
 project(samc21_cmsis_gcc C)
 
 set(CMAKE_C_STANDARD 99)
 
 include_directories(
-        ${CMAKE_CURRENT_LIST_DIR}/../Core
-        ${CMAKE_CURRENT_LIST_DIR}/../Core/Include
         ${CMAKE_CURRENT_LIST_DIR}
+        ${CMAKE_CURRENT_LIST_DIR}/Core
+        ${CMAKE_CURRENT_LIST_DIR}/Core/Include
         ${CMAKE_CURRENT_LIST_DIR}/include
         ${CMAKE_CURRENT_LIST_DIR}/include/component
         ${CMAKE_CURRENT_LIST_DIR}/include/pio
