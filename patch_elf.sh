71eb500473400d25aef910e9370524da23784e28
ben <bnoack@techshot.com>
2020-09-03 17:08:23 -0400

    patching elves

---
+++ patch_elf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+set -u
+# this script patches the Entry point address in the ELF header to the correct stack pointer address.
+# for some reason, the current GCC comes up with 0x00 as the SP!
+# So take the correct SP from the binary and patch it into the ELF...
+ELF=$1
+BIN=$2
+
+SP=$(xxd -c 4 -l 4 -s 0x00000004 -e $BIN | cut -d ' ' -f 2)
+echo "SP: $SP"
+# address calculated from ELF header spec
+echo "00000018: $(echo -n $SP | tac -rs ..)" | xxd -r - $ELF
+arm-none-eabi-readelf -h $ELF | grep "Entry point address"
+exit 0
