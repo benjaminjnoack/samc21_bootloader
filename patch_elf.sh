2d925210cd7d6cfbc5c8c7b90db1fe024264065d
ben <bnoack@techshot.com>
2020-09-04 09:27:12 -0400

    PC

--- patch_elf.sh
+++ patch_elf.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -e
 set -u
-# this script patches the Entry point address in the ELF header to the correct stack pointer address.
+# this script patches the Entry point address in the ELF header to the correct program counter address.
 # For some reason, the current GCC stores 0x00 in the ELF header Entry point address!
 # This causes debuggers like Ozone to set the PC to 0x00; consequently, the processor starts executing garbage.
-# So take the correct SP from the binary and patch it into the ELF...
+# So take the correct PC from the binary and patch it into the ELF...
 ELF=$1
 BIN=$2
 
-SP=$(xxd -c 4 -l 4 -s 0x00000004 $BIN | cut -d ' ' -f 2)
+PC=$(xxd -c 4 -l 4 -s 0x00000004 $BIN | cut -d ' ' -f 2)
 # address calculated from ELF header spec
-echo "00000018: $(echo -n $SP)" | xxd -r - $ELF
+echo "00000018: $(echo -n $PC)" | xxd -r - $ELF
 arm-none-eabi-readelf -h $ELF | grep "Entry point address"
 exit 0
